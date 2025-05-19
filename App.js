3db8109e047140444ee2e168dd86288519d59675
RobinBroberg <robin.broberg86@gmail.com>
2025-05-19 22:25:13 +0200

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [guess, setGuess] = useState("");
+  const [message, setMessage] = useState("");
+  const [target] = useState(Math.floor(Math.random() * 100) + 1);
+
+  function handleGuess() {
+    const number = Number(guess);
+
+    if (number < target) {
+      setMessage("För lågt");
+    } else if (number > target) {
+      setMessage("För högt");
+    } else if (number === target) {
+      setMessage("Snyggt, du hade rätt!");
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Gissa ett nummer mellan 1 och 100</Text>
+      <View style={styles.row}>
+        <TextInput
+          style={styles.input}
+          keyboardType="number-pad"
+          value={guess}
+          onChangeText={setGuess}
+        ></TextInput>
+        <Button title="Gissa" onPress={handleGuess}></Button>
+      </View>
+      <Text>{message}</Text>
     </View>
   );
 }
@@ -13,8 +38,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    margin: 10,
+    borderWidth: 1,
+    padding: 10,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
